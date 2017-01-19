8b791204bd4f33e7d0db09a58741512898f637f6
Hakan Bilgin <hbi99@hotmail.com>
2017-01-19 14:50:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
 if (!String.prototype.sprintf) {
 	String.prototype.sprintf = function () {
-		var args = Array.prototype.slice.call(arguments),
-			str = this.replace(/%s/g, function(r, i, s) {
-				return args.shift();
-			});
-		return str;
+		var args = Array.prototype.slice.call(arguments);
+		return this.replace(/%s/g, function() { return args.shift();});
 	};
 }
 
